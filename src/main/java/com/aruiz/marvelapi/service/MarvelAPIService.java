101879038885c840ba2dd29e7472d7429e8f7ef8
Arturo Ruiz <arturo.email>
2023-10-27 04:07:05 -0600

    Modifica clase service

--- src/main/java/com/aruiz/marvelapi/service/MarvelAPIService.java
+++ src/main/java/com/aruiz/marvelapi/service/MarvelAPIService.java
@@ -1,11 +1,13 @@
 package com.aruiz.marvelapi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import com.aruiz.marvelapi.client.MarvelAPIClient;
 
 @Component
 public class MarvelAPIService {
 
+	@Autowired
 	private MarvelAPIClient client;
 
 	public Object consultaHeroe(Integer idHeroe) {
